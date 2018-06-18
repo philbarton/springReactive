89d309b68e059d0a7c0ee45ae344bb5935add8a3
Phil Barton <philbart@mac.com>
2018-06-18 10:29:34 +0100

    readme

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # springReactive
+
+## Start Mongo using docker
+````
+docker run --name my-local-mongo -v mdata:/data/db -p 27017:27017 -d mongo
+````
+
+## Run from command line
+````
+ mvn spring-boot:run
+ ````
--- src/main/java/uk/co/maclon/claimant/model/Claimant.java
+++ src/main/java/uk/co/maclon/claimant/model/Claimant.java
@@ -4,6 +4,7 @@ import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 
 @Builder(toBuilder = true)
